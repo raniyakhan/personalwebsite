0a25e7e2b5aca63d7622b031863f76efb1ff8521
Raniya Khan <raniyakhan@berkeley.edu>
2024-08-09 21:21:29 -0400

    mild changes but its still very ugly

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     <div>
       {/* <h2>About</h2> */}
       <p>Hello!👋 I&apos;m Raniya (raan-yah), a rising sophomore at UC Berkeley 🧸 majoring in computer science 👩‍💻 and applied math 🧮. </p>
-      <p>I&apos;m really interested in the ai/ml/ds space, with the end-goal of making a meaningful social impact with tech.</p>
+      <p>I&apos;m really interested in the ai/ml/swe space, with the end-goal of making a meaningful social impact with tech.</p>
       <p>When I&apos;m not working, you&apos;ll find me playing NY Times word games or crocheting something new!</p>
     </div>
   );
